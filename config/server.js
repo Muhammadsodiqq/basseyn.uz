78826a6e55d001a174f73af6b4e37fffcdcf8143
Muhammad <ummatimuhammadiyy@gmail.com>
2021-09-09 11:14:31 +0500

    add repo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c52f3c0fb50202fff71acb543b304c47'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '43aceb80-2c62-4b21-8006-c9ba17dbfd3c'
+};
