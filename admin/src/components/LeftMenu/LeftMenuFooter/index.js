fbdbf0d5a3634a28d6eee560b5898a51d03566dc
Muhammad <ummatimuhammadiyy@gmail.com>
2021-09-11 15:37:51 +0500

    cdcd

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import Wrapper, { A } from './Wrapper';
+
+function LeftMenuFooter({ version }) {
+  // PROJECT_TYPE is an env variable defined in the webpack config
+  // eslint-disable-next-line no-undef
+  const projectType = PROJECT_TYPE;
+
+  return (
+   <></>
+  );
+}
+
+LeftMenuFooter.propTypes = {
+  version: PropTypes.string.isRequired,
+};
+
+export default LeftMenuFooter;
